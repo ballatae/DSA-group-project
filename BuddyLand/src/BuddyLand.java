cb68350c871ac17101f59f5486a94e188ff2f4ad
ETNIK\Admin <eballata@york.citycollege.eu>
2022-11-11 00:29:29 +0200

    removed unused imports in the interface

--- BuddyLand/src/BuddyLand.java
+++ BuddyLand/src/BuddyLand.java
@@ -5,36 +5,33 @@ public class BuddyLand {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BuddyList application = new BuddyList();
-		
-		
+
 		Buddy etnik = new Buddy("etnik", "ballata", "etnikballata@gmail.com", "kiki", "KiliMili");
 		Buddy tringa = new Buddy("tringa", "rexhepaj", "tringarexhepaj@gmail.com", "tringa", "Shylybyli");
 		Buddy sotiria = new Buddy("sotiria", "karvounidou", "sotiriakarvounidou@gmail.com", "sotiria", "Shangarepa");
 		Buddy elisavet = new Buddy("elisavet", "hassapi", "elisavethassapi@gmail.com", "elisavet", "Domate");
-		
+
 		application.addBuddy(etnik);
 		application.addBuddy(tringa);
 		application.addBuddy(sotiria);
 		application.addBuddy(elisavet);
-		
+
 		application.addFriend(etnik, tringa);
 		application.addFriend(etnik, elisavet);
-	
+
 		System.out.println(application.getBuddiesNumber(etnik));
-		
+
 		application.removeFriend(etnik, elisavet);
-		
+
 		System.out.println(application.getBuddiesNumber(tringa));
-		
+
 		try {
 			System.out.println(application.searchBuddyByName(etnik, "elisavet"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
 
-	
+	}
 
 }
--- BuddyLand/src/BuddyListInterface.java
+++ BuddyLand/src/BuddyListInterface.java
@@ -1,16 +1,13 @@
-import java.util.List;
-import java.util.Map;
 
 public interface BuddyListInterface {
 
-	
 	public void addBuddy(Buddy buddy);
-	
+
 	public void addFriend(Buddy buddyA, Buddy buddyB);
-	
+
 	public void removeFriend(Buddy buddyA, Buddy buddyB);
-	
+
 	public String searchBuddyByName(Buddy buddy, String name) throws Exception;
-	
+
 	public int getBuddiesNumber(Buddy buddy);
 }
